e9e567f7c3f4d1f987343f2ca42d234041284221
Rhadamez <rhadamez@gmail.com>
2019-10-23 15:16:21 -0300

    Adicionando paginação em Lancamento

--- src/main/java/com/rhadamez/mezicashapi/repository/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/com/rhadamez/mezicashapi/repository/lancamento/LancamentoRepositoryQuery.java
@@ -1,12 +1,13 @@
 package com.rhadamez.mezicashapi.repository.lancamento;
 
-import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 
 import com.rhadamez.mezicashapi.model.Lancamento;
 import com.rhadamez.mezicashapi.repository.filter.LancamentoFilter;
 
 public interface LancamentoRepositoryQuery {
 
-	public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+	public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 	
 }
--- src/main/java/com/rhadamez/mezicashapi/resource/LancamentoResource.java
+++ src/main/java/com/rhadamez/mezicashapi/resource/LancamentoResource.java
@@ -1,5 +1,6 @@
 package com.rhadamez.mezicashapi.resource;
 
+
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
@@ -11,6 +12,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -49,8 +52,8 @@ public class LancamentoResource {
 	private ApplicationEventPublisher publisher;
 
 	@GetMapping
-	public List<Lancamento> pesquisar(LancamentoFilter lancamentoFilter) {
-		return lancamentoRepository.filtrar(lancamentoFilter);
+	public Page<Lancamento> pesquisar(LancamentoFilter lancamentoFilter, Pageable pageable) {
+		return lancamentoRepository.filtrar(lancamentoFilter, pageable);
 	}
 
 	@GetMapping("/{id}")
